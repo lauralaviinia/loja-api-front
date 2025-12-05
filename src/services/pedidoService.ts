045ef607f5dd364be0d0e9d1526ed8cab00d070e
Laura Lavínia <134345628+lauralaviinia@users.noreply.github.com>
2025-12-05 08:48:03 -0300

    fix pedidoService.ts

--- src/services/pedidoService.ts
+++ src/services/pedidoService.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import type { Pedido } from "../types/pedido";
 
-const API_BASE = "http://localhost:3000";
-
 // ===============================
 // Mapeamento
 // ===============================
