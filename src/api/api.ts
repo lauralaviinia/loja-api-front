a09b4ecc5c64f8f470924266ae94af104b5f2afa
Laura Lavínia <134345628+lauralaviinia@users.noreply.github.com>
2025-12-05 08:04:35 -0300

    fix api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.PROD
-    import.meta.env.VITE_API_URL || "https://loja-api-back.onrender.com", // Render
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL || "https://loja-api-back.onrender.com", // Render
     heards: {
       "Content-Type": "application/json",
     },                      
