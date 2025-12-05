75d9513282c54b8071f30beb07fc9ac2d6677d53
Laura Lavínia <134345628+lauralaviinia@users.noreply.github.com>
2025-12-05 08:05:45 -0300

    fix api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "https://loja-api-back.onrender.com", // Render
-    heards: {
+    headers: {
       "Content-Type": "application/json",
     },                      
 });
